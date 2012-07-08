d4dcc1b28ed2e005274f1193833004e17a50bd5f
emb <emb@catfact.net>
2012-07-07 21:09:34 -0400

    added current pinmux document for avr32 evk

--- avr32/config.mk
+++ avr32/config.mk
@@ -105,7 +105,7 @@ DBGFLAGS =
 
 # Application optimization used during compilation and linking:
 # -O0, -O1, -O2, -O3 or -Os
-OPTIMIZATION = -O1
+OPTIMIZATION = -O2
 
 # Extra flags to use when archiving.
 ARFLAGS = 
--- avr32/src/main.c
+++ avr32/src/main.c
@@ -23,72 +23,8 @@
 #include "events.h"
 #include "files.h"
 #include "init.h"
+#include "interrupts.h"
 #include "screen.h"
-
-
-//------------------------
-//----- variables
-// end of PDCA transfer
-volatile bool end_of_transfer;
-
-//----------------------
-//---- static functions 
-
-// interrupt handlers
-__attribute__((__interrupt__))
-static void irq_pdca(void) {
-  volatile U16 delay;
-  // Disable all interrupts.
-  Disable_global_interrupt();
-
-  // Disable interrupt channel.
-  pdca_disable_interrupt_transfer_complete(AVR32_PDCA_CHANNEL_SPI_RX);
-
-  sd_mmc_spi_read_close_PDCA();//unselects the SD/MMC memory.
-
-  // wait (FIXME)
-  delay=0; while(delay < 5000) { delay++; }
-
-  // Disable unnecessary channel
-  pdca_disable(AVR32_PDCA_CHANNEL_SPI_TX);
-  pdca_disable(AVR32_PDCA_CHANNEL_SPI_RX);
-
-  // Enable all interrupts.
-  Enable_global_interrupt();
-
-  end_of_transfer = true;
-}
-
-/// debug
-static U32 dum = 0;
-
-// interrupt handler for PB00-PB07
-__attribute__((__interrupt__))
-static void irq_port0_line0(void) {
-  // BFIN_HWAIT: set value
-  if(gpio_get_pin_interrupt_flag(BFIN_HWAIT_PIN)) {
-    if ((dum++ % 2000) == 0) { 
-      gpio_tgl_gpio_pin(LED2_GPIO);
-    }
-    hwait = gpio_get_pin_value(BFIN_HWAIT_PIN);
-    gpio_clear_pin_interrupt_flag(BFIN_HWAIT_PIN);
-  }
-}
-
-// register interrupts
-static void register_interrupts(void) {
-  // generate an interrupt when bfin HWAIT changes
-  gpio_enable_pin_interrupt( BFIN_HWAIT_PIN, GPIO_PIN_CHANGE);
-  
-  // register IRQ for port B, 0-7
-  //  INTC_register_interrupt( &irq_port1_line0, AVR32_GPIO_IRQ_0 + (AVR32_PIN_PB00 / 8), AVR32_INTC_INT1 );
-  
-  // register IRQ for port A, 0-7
-  INTC_register_interrupt( &irq_port0_line0, AVR32_GPIO_IRQ_0 + (AVR32_PIN_PA00 / 8), AVR32_INTC_INT1 );
-  
-  // register IRQ for PDCA transfer
-  INTC_register_interrupt(&irq_pdca, AVR32_PDCA_IRQ_0, AVR32_INTC_INT1); 
-}
 
 ////main function
 int main (void) {
