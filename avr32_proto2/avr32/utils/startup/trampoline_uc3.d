cafea6ce7bcd251c8668e5d05697e140e7a6344d
emb <emb@catfact.net>
2012-11-30 18:52:40 -0500

    clean out build files

--- avr32_proto2/avr32/utils/startup/trampoline_uc3.d
+++
@@ -1,5 +0,0 @@
-avr32/utils/startup/trampoline_uc3.o: \
- ../../avr/asf-3.1.3/avr32/utils/startup/trampoline_uc3.S \
- ../../avr/asf-3.1.3/avr32/utils/startup/trampoline_uc3.h
-
-../../avr/asf-3.1.3/avr32/utils/startup/trampoline_uc3.h:
