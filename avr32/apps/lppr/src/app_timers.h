145e44384cb3ca633b6c804c7d77494a740dad9d
emb <emb@catfact.net>
2013-09-08 15:37:21 -0700

    add app timers

---
+++ avr32/apps/lppr/src/app_timers.h
@@ -0,0 +1,19 @@
+#ifndef _ALEPH_AVR32_APP_TIMERS_H_
+#define _ALEPH_AVR32_APP_TIMERS_H_
+
+#include "sys_timers.h"
+
+// ---- timer tags
+enum { 
+  eScreenTimerTag = eNumSysTimers,
+  //  eAdcTimerTag,
+  //  eMonomePollTimerTag,
+  //  eMonomeRefreshTimerTag,
+  //  eMidiPollTimerTag,
+  //  eMidiRefreshTimerTag,
+};
+
+// create application timers
+extern void init_app_timers(void);
+
+#endif
