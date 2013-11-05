1cbdc227ac90f0b278365a6aa0374041aedfe1e3
emb <emb@catfact.net>
2013-11-05 14:15:11 -0500

    moved stuff, fixed nothing

--- tests/spi_boot/avr32/src/init.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INIT_AVR_H_
-#define _INIT_AVR_H_
-
-#include "compiler.h"
-
-extern void init_avr(void); 
-
-#endif // _INIT_AVR_H_
--- tests/ui/avr32/src/ui.h
+++
@@ -1,6 +0,0 @@
-#ifndef _UI_H_
-#define _UI_H_
-
-void 
-
-#endif // header guard
