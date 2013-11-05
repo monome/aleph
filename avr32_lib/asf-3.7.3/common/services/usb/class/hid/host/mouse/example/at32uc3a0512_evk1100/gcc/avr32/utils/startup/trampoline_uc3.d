1589b364ae86ff018ffc01957f6594b2dc5d9c39
emb <emb@catfact.net>
2013-11-05 15:24:39 -0500

    asf: strip examples

--- avr32_lib/asf-3.7.3/common/services/usb/class/hid/host/mouse/example/at32uc3a0512_evk1100/gcc/avr32/utils/startup/trampoline_uc3.d
+++
@@ -1,5 +0,0 @@
-avr32/utils/startup/trampoline_uc3.o: \
- ../../../../../../../../../../avr32/utils/startup/trampoline_uc3.S \
- ../../../../../../../../../../avr32/utils/startup/trampoline_uc3.h
-
-../../../../../../../../../../avr32/utils/startup/trampoline_uc3.h:
