23d78b4fdb3a8089c629c3a944265e21467b3e75
emb <emb@catfact.net>
2013-11-13 11:37:37 -0800

    fix dead includes in apps

--- apps/lppr/src/app_timers.h
+++ apps/lppr/src/app_timers.h
@@ -1,19 +1,6 @@
 #ifndef _ALEPH_AVR32_APP_TIMERS_H_
 #define _ALEPH_AVR32_APP_TIMERS_H_
 
-#include "sys_timers.h"
-
-// ---- timer tags
-enum { 
-  eScreenTimerTag = eNumSysTimers,
-  eMetroTimerTag,
-  eMonomePollTimerTag,
-  eMonomeRefreshTimerTag,
-  //  eAdcTimerTag,
-  //  eMidiPollTimerTag,
-  //  eMidiRefreshTimerTag,
-};
-
 // create application timers
 extern void init_app_timers(void);
 
