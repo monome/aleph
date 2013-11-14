3153263bb52e932fd8697a0e3119318530edf1df
emb <emb@catfact.net>
2013-11-14 13:04:05 -0800

    clean up param value types, fix compile in modules/lines

--- dsp/module.h
+++ dsp/module.h
@@ -52,7 +52,7 @@ extern void module_process_frame(const f32* in, f32* out);
 #endif
 
 // set parameter  
-extern void module_set_param(u32 idx, pval val);
+extern void module_set_param(u32 idx, ParamValue val);
 // get number of parameters
 extern u32 module_get_num_params(void);
 
