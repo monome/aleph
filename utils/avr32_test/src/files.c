4ebd7162933e7876229fdfaf94b686e925b54705
tehn <tehn@monome.org>
2013-11-14 14:02:02 -0500

    check for dsp in /mod

--- utils/avr32_test/src/files.c
+++ utils/avr32_test/src/files.c
@@ -12,7 +12,7 @@
 // lppr
 #include "files.h"
 
-#define LPPR_LDR_PATH "/dsp/aleph-dacs.ldr"
+#define LPPR_LDR_PATH "/mod/aleph-dacs.ldr"
 
 // search for our .ldr file in the filesystem
 // load and store in internal flash if found, otherwise report failure
