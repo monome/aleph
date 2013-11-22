66d9093bc9203c5501d1497d265dcd02ad590a9c
brian crabtree <tehn@monome.org>
2013-11-21 20:40:48 -0500

    uart getparam, cleanup, 32bit to 4 byte stuff

--- avr32_lib/src/serial.h
+++ avr32_lib/src/serial.h
@@ -7,11 +7,17 @@ typedef void(*process_serial_t)(u16);
 extern volatile u8 serial_buffer[SERIAL_BUFFER_SIZE];
 
 extern void serial_process(void);
+
 extern void serial_send_start(u8);
 extern void serial_send_byte(u8);
 extern void serial_send_long(u32);
 extern void serial_send_separator(void);
 extern void serial_send_end(void);
+
+extern void serial_param_num(s32 data);
+extern void serial_param_info(s32 data);
+extern void serial_param_get(s32 data);
+extern void serial_param_set(s32 data);
 
 
 // incoming serial:
