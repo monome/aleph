2012ffc51757205924a916e92eb94a291b386e6c
emb <emb@catfact.net>
2013-11-23 18:17:26 -0800

    scalers progress

--- apps/bees/src/scaler.c
+++ apps/bees/src/scaler.c
@@ -12,18 +12,28 @@
 // type-specific
 #include "scalers/scaler_amp.h"
 
+// class structure
+typedef struct _scaler { 
+  // init
+  // de-init
+  // set-scale 
+  // ui-scalel
+  // tuning
+  
+} scaler;
 
-// array of poitners to initialization functions.
-void(*scaler_init_pr[])(fix16 min, fix16 max) = {
+// array of pointers to initialization functions.
+void(*scaler_init_pr[eParamNumTypes])(fix16 min, fix16 max) = {
+  
 };
 
 // array of pointers to scaling functions.
-s32 (*scaler_val_pr[])(io_t in) = {
+s32 (*scaler_val_pr[eParamNumTypes])(io_t in) = {
   
 };
 
 // array of pointers to representation functions
-fix16 (*scaler_rep_pr[])(io_t in) = {
+fix16 (*scaler_rep_preParamNumTypes[])(io_t in) = {
 };
 
 // initialization flags
