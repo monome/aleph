3f73be33503ddebb72bc6c4c14ea2c13b6baa197
rick-monster <richard.venn@gmail.com>
2014-03-09 14:28:12 +0000

    gradually getting rid of all beekeep source from the bees-linux buildtarget

--- utils/avr32_sim/asf-3.7.3/avr32/drivers/gpio/gpio.c
+++ utils/avr32_sim/asf-3.7.3/avr32/drivers/gpio/gpio.c
@@ -2,3 +2,9 @@
 void gpio_set_gpio_pin( uint32_t pin) {
     return;
 }
+void gpio_clr_gpio_pin(uint32_t pin){
+    return;
+}
+int gpio_get_pin_value(uint32_t pin) {
+    return 1;
+}
--- utils/avr32_sim/asf-3.7.3/avr32/drivers/gpio/gpio.h
+++ utils/avr32_sim/asf-3.7.3/avr32/drivers/gpio/gpio.h
@@ -1,3 +1,4 @@
 #include "types.h"
-#define POWER_CTL_PIN 0
 void app_gpio_set_gpio_pin(uint32_t pin);
+void gpio_clr_gpio_pin(uint32_t pin);
+bool gpio_get_pin_value(uint32_t pin);
