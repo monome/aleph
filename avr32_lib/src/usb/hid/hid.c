b617bbf079b68033717dc8cd3d0b5d133b58cfb6
emb <emb@catfact.net>
2014-04-16 14:04:51 -0700

    avr32_lib: HID event now ust transmits bitfield indicating which bytes have changed in the frame.

--- avr32_lib/src/usb/hid/hid.c
+++ avr32_lib/src/usb/hid/hid.c
@@ -13,27 +13,48 @@
 #define HID_FRAME_MAX_BYTES 8
 
 static u8 frame[HID_FRAME_MAX_BYTES];
-static event_t ev;
+static event_t ev = { .type = kEventHidPacket };
+
+
+// set/unset dirty flag for given byte in frame
+static void hid_set_byte_flag(u32* data, u8 byte, u8 val) {
+  if(val) {
+    (*data) |= (1 << byte);
+  } else {
+    (*data) &= 0xffffffff ^ (1 << byte);
+  }
+}
+
+// test dirty flag for given byte in packet
+u8 hid_get_byte_flag(s32 data, u8 byte) {
+  return (data & (1 << byte)) > 0;
+}
 
 // parse frame and spawn events
 void hid_parse_frame(u8* data, u8 size) {
   u8 i;
-
-  // FIXME: one event per byte changed, pass byte idx and value as event data.
-  // maybe this is not really too bad since most HID controls are only one byte.
+  // one event per frame changed.
+  // event data is bitfield indicating bytes changed.
   for(i=0; i<size; i++) {
-    if(data[i] != frame[i]) {
-      frame[i] = data[i];
-      ev.type = kEventHidPacket;
-      ev.data = frame[i];
-      ev.data |= (i << 8);
-      event_post(&ev);
-    }
+    hid_set_byte_flag( &(ev.data), i, data[i] != frame[i] );
   }
+  event_post(&ev);
+
+  /* // one event per byte changed. */
+  /* for(i=0; i<size; i++) { */
+  /*   if(data[i] != frame[i]) { */
+  /*     frame[i] = data[i]; */
+  /*     ev.type = kEventHidPacket; */
+      
+  /*     /\* ev.data = frame[i]; *\/ */
+  /*     /\* ev.data |= (i << 8); *\/ */
+  /*     event_post(&ev); */
+  /*   } */
+  /* } */
 }
 
 // get the frame data 
-const u8* hid_get_frame(void) {
+const u8* hid_get_frame_data(void) {
   return (const u8*)frame;
 }
 
