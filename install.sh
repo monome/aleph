c788d88f14ee076d49b7dd2676d15b76e566cc1d
ezra <emb@catfact.net>
2014-10-04 17:44:05 -0700

    added docs to dev branch, not sure when/how they disappeared...

---
+++ install.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+mkdir $1/app
+mkdir $1/mod
+mkdir $1/data
+mkdir $1/data/bees
+mkdir $1/data/bees/scalers
+mkdir $1/data/bees/scenes/
+
+cp utils/param_scaling/scaler_*.dat $1/data/bees/scalers/
+cp apps/bees/aleph-bees*.hex $1/app/
+cp modules/waves/aleph-waves.ldr $1/mod/
+cp modules/waves/aleph-waves.dsc $1/mod/
+cp modules/lines/aleph-lines.ldr $1/mod/
+cp modules/lines/aleph-lines.dsc $1/mod/
