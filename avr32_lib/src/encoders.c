d4915eabdf03350365bc0615c620e9f29b18e7f2
brian crabtree <tehn@monome.org>
2014-10-16 11:25:35 -0400

    ww fixes

--- avr32_lib/src/encoders.c
+++ avr32_lib/src/encoders.c
@@ -70,7 +70,7 @@ void set_enc_thresh( const u8 idx, const u8 v ) {
   print_dbg("\r\n setting encoder threshold, idx: ");
   print_dbg_hex(idx);
   print_dbg(" ; value: ");
-  print_dbg_hex(v), 
+  print_dbg_hex(v);
 
   enc[idx].thresh = v;
   // clear the value so we don't trigger spurious event on next poll!
