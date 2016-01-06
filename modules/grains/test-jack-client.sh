20aa8246a6721e4e4f02ab88916eca4decda9152
The Hairy One <sasquatch@rickvenn.com>
2016-01-06 12:35:06 -0800

    fix up bfin-emu to coexist with bfin build

--- modules/grains/test-jack-client.sh
+++ modules/grains/test-jack-client.sh
@@ -1,2 +1,2 @@
-gcc -ljack -lm fract32_jack.c jack_pitch_tracker.c ../../dsp/ricks_tricks.c -I ../../dsp -I ./ -o jack_pitch_tracker;
+gcc -ljack -lm bfin_emu/fract32_jack.c bfin_emu/jack_pitch_tracker.c ../../dsp/ricks_tricks.c -I ../../dsp -I ./bfin_emu -I ./ -o jack_pitch_tracker;
 # ./jack_pitch_tracker
