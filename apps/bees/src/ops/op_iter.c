bdb16b524df618eaf0ac281dc10e696fb2dbeeaa
The Hairy One <sasquatch@rickvenn.com>
2016-02-21 23:54:19 -0800

    smash op_add into op_iter

--- apps/bees/src/ops/op_iter.c
+++ apps/bees/src/ops/op_iter.c
@@ -3,9 +3,9 @@
 
 //-------------------------------------------------
 //---- static func declare
-static void op_iter_in_a(op_iter_t* iter, const io_t v);
-static void op_iter_in_b(op_iter_t* iter, const io_t v);
-static void op_iter_in_btrig(op_iter_t* iter, const io_t v);
+static void op_iter_in_times(op_iter_t* iter, const io_t v);
+static void op_iter_in_val(op_iter_t* iter, const io_t v);
+static void op_iter_in_stop(op_iter_t* iter, const io_t v);
 
 // pickle / unpickle
 static u8* op_iter_pickle(op_iter_t* op, u8* dst);
@@ -13,14 +13,14 @@ static const u8* op_iter_unpickle(op_iter_t* op, const u8* src);
 
 //-------------------------------------------------
 //---- static vars
-static const char* op_iter_instring = "A\0      B\0      TRIG\0   ";
-static const char* op_iter_outstring = "SUM\0    ";
+static const char* op_iter_instring  = "TIMES\0  VAL\0    STOP\0   ";
+static const char* op_iter_outstring = "IDX\0    VAL\0    DONE\0   ";
 static const char* op_iter_opstring = "ITER";
 
 static op_in_fn op_iter_in_fn[3] = {
-  (op_in_fn)&op_iter_in_a,
-  (op_in_fn)&op_iter_in_b, 
-  (op_in_fn)&op_iter_in_btrig
+  (op_in_fn)&op_iter_in_times,
+  (op_in_fn)&op_iter_in_val,
+  (op_in_fn)&op_iter_in_stop
 };
 
 //-------------------------------------------------
@@ -28,8 +28,10 @@ static op_in_fn op_iter_in_fn[3] = {
 void op_iter_init(void* mem) {
   op_iter_t* iter = (op_iter_t*)mem;
   iter->super.numInputs = 3;
-  iter->super.numOutputs = 1;
+  iter->super.numOutputs = 3;
   iter->outs[0] = -1;
+  iter->outs[1] = -1;
+  iter->outs[2] = -1;
 
   iter->super.in_fn = op_iter_in_fn;
   iter->super.in_val = iter->in_val;
@@ -42,47 +44,52 @@ void op_iter_init(void* mem) {
   iter->super.outString = op_iter_outstring;
   iter->super.type = eOpIter;  
 
-  iter->in_val[0] = &(iter->a);
-  iter->in_val[1] = &(iter->b);
-  iter->in_val[2] = &(iter->btrig);
+  iter->in_val[0] = &(iter->times);
+  iter->in_val[1] = &(iter->val);
+  iter->in_val[2] = &(iter->stop);
 
-  iter->a = 0;
-  iter->b = 0;
-  iter->btrig = 0;
+  iter->times = 0;
+  iter->val = 0;
+  iter->stop = 0;
 
 }
 
 //-------------------------------------------------
 //---- static func define
 
-static void op_iter_in_a(op_iter_t* iter, const io_t v) {
-  iter->a = v;
-  net_activate(iter->outs[0], iter->val, iter);
+static void op_iter_in_times(op_iter_t* iter, const io_t v) {
+  if (v >= 0 && v < 16)
+    iter->times = v;
 }
 
-static void op_iter_in_b(op_iter_t* iter, const io_t v) {
-  iter->b = v;
-  if(iter->btrig) {
-    net_activate(iter->outs[0], iter->val, iter);
+static void op_iter_in_val(op_iter_t* iter, const io_t v) {
+  u8 i = 0;
+  iter->stop = 0;
+  for (i=0; i < iter->times; i++) {
+    if ( iter->stop == 1)
+      return;
+    net_activate(iter->outs[0], i, iter);
+    net_activate(iter->outs[1], iter->val, iter);
   }
+  net_activate(iter->outs[2], 1, iter);
 }
 
-static void op_iter_in_btrig(op_iter_t* iter, const io_t v) {
-  if(v > 0) { iter->btrig = OP_ONE; } else { iter->btrig = 0; }
+static void op_iter_in_stop(op_iter_t* iter, const io_t v) {
+  iter->stop = 1;
 }
 
 
 // pickle / unpickle
 u8* op_iter_pickle(op_iter_t* op, u8* dst) {
-  dst = pickle_io(op->a, dst);
-  dst = pickle_io(op->b, dst);
-  dst = pickle_io(op->btrig, dst);
+  dst = pickle_io(op->times, dst);
+  dst = pickle_io(op->val, dst);
+  dst = pickle_io(op->stop, dst);
   return dst;
 }
 
 const u8* op_iter_unpickle(op_iter_t* op, const u8* src) {
-  src = unpickle_io(src, &(op->a));
-  src = unpickle_io(src, &(op->b));
-  src = unpickle_io(src, &(op->btrig));
+  src = unpickle_io(src, &(op->times));
+  src = unpickle_io(src, &(op->val));
+  src = unpickle_io(src, &(op->stop));
   return src;
 }
--- apps/bees/src/ops/op_iter.h
+++ apps/bees/src/ops/op_iter.h
@@ -8,12 +8,11 @@
 //--- op_iter_t : iterition
 typedef struct op_iter_struct {
   op_t super;
-  volatile io_t val; 
-  volatile io_t a;
-  volatile io_t b;
-  volatile io_t btrig;
+  volatile io_t times;
+  volatile io_t val;
+  volatile io_t stop;
   volatile io_t * in_val[3]; // a, b, btrig
-  op_out_t outs[1];
+  op_out_t outs[3];
 } op_iter_t;
 void op_iter_init(void* mem);
 
