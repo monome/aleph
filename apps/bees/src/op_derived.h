1e4bd19c8f6916c1f0beec8dcf365908ccd64612
UrTih the Alchemist <UrTih@planet.thra>
2016-11-06 19:04:10 -0800

    rename op_life to op_life_classic

--- apps/bees/src/op_derived.h
+++ apps/bees/src/op_derived.h
@@ -30,7 +30,7 @@
 #include "ops/op_history.h"
 #include "ops/op_is.h"
 #include "ops/op_iter.h"
-#include "ops/op_life.h"
+#include "ops/op_life_classic.h"
 #include "ops/op_list2.h"
 #include "ops/op_list8.h"
 #include "ops/op_list16.h"
