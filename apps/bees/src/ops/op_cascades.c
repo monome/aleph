fdabba839cbeb8f8bcb9f09a7aa6cdf64c68fefe
Rick Venn <sasquatch@rickvenn.com>
2017-04-18 19:54:04 -0700

    pickling meadowphysics state

--- apps/bees/src/ops/op_cascades.c
+++ apps/bees/src/ops/op_cascades.c
@@ -14,12 +14,6 @@
 //-------------------------------------------------
 //----- static variables
 
-/////////////////
-/// FIXME:
-static io_t dummy = 0;
-///////////////
-
-
 //---- descriptor strings
 static const char* op_cascades_instring = "FOCUS\0  SIZE\0   STEP\0   ";
 static const char* op_cascades_outstring ="OUT0\0   OUT1\0   OUT2\0   OUT3\0   OUT4\0   OUT5\0   OUT6\0   OUT7\0   ";
@@ -43,7 +37,7 @@ static const u8* op_cascades_unpickle(op_cascades_t* enc, const u8* src);
 static void op_cascades_handler(op_monome_t* op_monome, u32 data);
 
 static void op_cascades_redraw(op_monome_t* op_monome);
-static void op_cascades_trigger(u8 n);
+static void op_cascades_trigger(op_cascades_t* op_monome, u8 n);
 
 static u32 rnd(void);
 
@@ -54,18 +48,6 @@ static op_in_fn op_cascades_in_fn[3] = {
   (op_in_fn)&op_cascades_in_step,
 };
 
-
-static s8 positions[8] = {3,1,2,2,3,3,5,7};
-static s8 points[8] = {3,1,2,2,3,3,5,7};
-static s8 points_save[8] = {3,1,2,2,3,3,5,7};
-static u8 triggers[8] = {0,0,0,0,0,0,0,0};
-static u8 trig_dests[8] = {0,0,0,0,0,0,0,0};
-static u8 rules[8] = {0,0,0,0,0,0,0,0};
-static u8 rule_dests[8] = {0,1,2,3,4,5,6,7};
-
-static u8 edit_row, key_count = 0, mode = 0, prev_mode = 0;
-
-static u8 XSIZE = 16;
 
 const u8 glyph[8][8] = {{0,0,0,0,0,0,0,0},         // o
        {0,24,24,126,126,24,24,0},     // +
@@ -84,8 +66,22 @@ u32 a2 = 0x19660d;
 u32 c2 = 0x3c6ef35f;
 u32 x2 = 7654321;  // seed
 
+void cascades_copy_init_u(u8 *dest, u8 *src);
+void cascades_copy_init_s(s8 *dest, s8 *src);
 
+void cascades_copy_init_u(u8 *dest, u8 *src) {
+  int i;
+  for(i=0; i<8; i++) {
+    dest[i] = src[i];
+  }
+}
 
+void cascades_copy_init_s(s8 *dest, s8 *src) {
+  int i;
+  for(i=0; i<8; i++) {
+    dest[i] = src[i];
+  }
+}
 //-------------------------------------------------
 //----- extern function definition
 void op_cascades_init(void* mem) {
@@ -122,7 +118,7 @@ void op_cascades_init(void* mem) {
   op->in_val[1] = &(op->size);  
   ///////////////////
   // FIXME
-  op->in_val[2] = &dummy;  
+  op->in_val[2] = &(op->dummy);  
   ////////////////////
 
   op->outs[0] = -1;
@@ -133,10 +129,29 @@ void op_cascades_init(void* mem) {
   op->outs[5] = -1;
   op->outs[6] = -1;
   op->outs[7] = -1;
+  op->key_count = 0;
+  op->mode = 0;
+  op->prev_mode = 0;
 
-  edit_row = key_count = mode = prev_mode = 0;
+  op->edit_row = op->key_count = op->mode = op->prev_mode = 0;
 
   op->size = monome_size_x();
+  op->dummy = 0;
+
+  s8 positions_init[8] = {3,1,2,2,3,3,5,7};
+  cascades_copy_init_s(op->positions, positions_init);
+  s8 points_init[8] = {3,1,2,2,3,3,5,7};
+  cascades_copy_init_s(op->points, points_init);
+  s8 points_save_init[8] = {3,1,2,2,3,3,5,7};
+  cascades_copy_init_s(op->points_save, points_save_init);
+  u8 triggers_init[8] = {0,0,0,0,0,0,0,0};
+  cascades_copy_init_u(op->triggers, triggers_init);
+  u8 trig_dests_init[8] = {0,0,0,0,0,0,0,0};
+  cascades_copy_init_u(op->trig_dests, trig_dests_init);
+  u8 rules_init[8] = {0,0,0,0,0,0,0,0};
+  cascades_copy_init_u(op->rules, rules_init);
+  u8 rules_dests_init[8] = {0,1,2,3,4,5,6,7};
+  cascades_copy_init_u(op->rule_dests, rules_dests_init);
 
   op->focus = OP_ONE;
   net_monome_set_focus(&(op->monome), 1);
@@ -170,7 +185,7 @@ static void op_cascades_in_size(op_cascades_t* op, const io_t v) {
   else op->size = 16;
 
   // FIXME: auto-detect size, this doesn't get set in unpickling
-  XSIZE = op->size;
+  op->XSIZE = op->size;
 }
 
 static void op_cascades_in_step(op_cascades_t* op, const io_t v) {
@@ -178,20 +193,20 @@ static void op_cascades_in_step(op_cascades_t* op, const io_t v) {
 
   // clear last round
   for(i=0;i<8;i++)
-    triggers[i] = 0;
+    op->triggers[i] = 0;
 
   // main
-  op_cascades_trigger(0);
+  op_cascades_trigger(op, 0);
 
   // ensure bounds, output triggers
   for(i=0;i<8;i++) {
-    if(positions[i] < 0)
-      positions[i] = 0;
-    else if(positions[i] > points[i])
-      positions[i] = points[i];
+    if(op->positions[i] < 0)
+      op->positions[i] = 0;
+    else if(op->positions[i] > op->points[i])
+      op->positions[i] = op->points[i];
 
     // send out
-    if(triggers[i]) {
+    if(op->triggers[i]) {
       net_activate(op, i, 1);
     }
   }
@@ -199,75 +214,76 @@ static void op_cascades_in_step(op_cascades_t* op, const io_t v) {
   op_cascades_redraw(&op->monome);
 }
 
-static void op_cascades_trigger(u8 n) {
+static void op_cascades_trigger(op_cascades_t *op, u8 n) {
   u8 m;
 
-  positions[n]--;
+  op->positions[n]--;
 
   // ****** the trigger # check is so we don't cause a trigger/rules multiple times per NEXT
   // a rules-based jump to position-point does not current cause a trigger. should it?
-  if(positions[n] < 0 && triggers[n] == 0) {
-    triggers[n]++;
+  if(op->positions[n] < 0 && op->triggers[n] == 0) {
+    op->triggers[n]++;
   
-    if(rules[n] == 1) {     // inc
-      if(points[rule_dests[n]] < (XSIZE-1)) {
-        points[rule_dests[n]]++;
-        positions[rule_dests[n]] = points[rule_dests[n]];
+    if(op->rules[n] == 1) {     // inc
+      if(op->points[op->rule_dests[n]] < (op->XSIZE-1)) {
+        op->points[op->rule_dests[n]]++;
+        op->positions[op->rule_dests[n]] = op->points[op->rule_dests[n]];
       }
     }
-    else if(rules[n] == 2) {  // dec
-      if(points[rule_dests[n]] > 0) {
-        points[rule_dests[n]]--;
-        positions[rule_dests[n]] = points[rule_dests[n]];
+    else if(op->rules[n] == 2) {  // dec
+      if(op->points[op->rule_dests[n]] > 0) {
+        op->points[op->rule_dests[n]]--;
+        op->positions[op->rule_dests[n]] = op->points[op->rule_dests[n]];
       }
     }
-    else if(rules[n] == 3) {  // max
-      points[rule_dests[n]] = (XSIZE-1);
-      positions[rule_dests[n]] = points[rule_dests[n]];
+    else if(op->rules[n] == 3) {  // max
+      op->points[op->rule_dests[n]] = (op->XSIZE-1);
+      op->positions[op->rule_dests[n]] = op->points[op->rule_dests[n]];
     }
-    else if(rules[n] == 4) {  // min
-      points[rule_dests[n]] = 0;
-      positions[rule_dests[n]] = points[rule_dests[n]];
+    else if(op->rules[n] == 4) {  // min
+      op->points[op->rule_dests[n]] = 0;
+      op->positions[op->rule_dests[n]] = op->points[op->rule_dests[n]];
     }
-    else if(rules[n] == 5) {  // rnd
-      points[rule_dests[n]] = rnd() % XSIZE;
+    else if(op->rules[n] == 5) {  // rnd
+      op->points[op->rule_dests[n]] = rnd() % op->XSIZE;
       
       print_dbg("\r\n op_cascades >>>>>>>>>>>>>>>>>>>> RANDOM: ");
-      print_dbg_hex(points[rule_dests[n]]);
+      print_dbg_hex(op->points[op->rule_dests[n]]);
       // print_dbg_hex(rnd() % 11);
 
-      positions[rule_dests[n]] = points[rule_dests[n]];
+      op->positions[op->rule_dests[n]] = op->points[op->rule_dests[n]];
     }
-    else if(rules[n] == 6) {  // up/down
-      points[rule_dests[n]] += rnd() % 3;
-      points[rule_dests[n]]--;
+    else if(op->rules[n] == 6) {  // up/down
+      op->points[op->rule_dests[n]] += rnd() % 3;
+      op->points[op->rule_dests[n]]--;
 
 
-      if(points[rule_dests[n]] < 0) points[rule_dests[n]] = 0;
-      else if(points[rule_dests[n]] > (XSIZE-1)) points[rule_dests[n]] = XSIZE-1;
-      positions[rule_dests[n]] = points[rule_dests[n]];  
+      if(op->points[op->rule_dests[n]] < 0) op->points[op->rule_dests[n]] = 0;
+      else if(op->points[op->rule_dests[n]] > (op->XSIZE-1)) op->points[op->rule_dests[n]] = op->XSIZE-1;
+      op->positions[op->rule_dests[n]] = op->points[op->rule_dests[n]];  
 
       print_dbg("\r\n op_cascades >>>>>>>>>>>>>>>>>>>> WANDER: ");
-      print_dbg_hex(points[rule_dests[n]]);   
+      print_dbg_hex(op->points[op->rule_dests[n]]);   
     }
-    else if(rules[n] == 7) {  // return
-      points[rule_dests[n]] = points_save[rule_dests[n]];
+    else if(op->rules[n] == 7) {  // return
+      op->points[op->rule_dests[n]] = op->points_save[op->rule_dests[n]];
     }
 
 
     //reset
-    positions[n] += points[n] + 1;
+    op->positions[n] += op->points[n] + 1;
 
     //triggers
     for(m=0;m<8;m++)
-      if((trig_dests[n] & (1<<m)) != 0)
-        op_cascades_trigger(m);
+      if((op->trig_dests[n] & (1<<m)) != 0)
+        op_cascades_trigger(op, m);
         // post("\ntrigger",n," -> ", m);
   }
 }
 
 // process monome key input
 static void op_cascades_handler(op_monome_t* op_monome, u32 edata) {
+  op_cascades_t *op = (op_cascades_t *) op_monome->op;
   static u8 x, y, z;
   u8 dirty_grid = 0;
 
@@ -275,56 +291,56 @@ static void op_cascades_handler(op_monome_t* op_monome, u32 edata) {
 
   monome_grid_key_parse_event_data(edata, &x, &y, &z);
 
-  prev_mode = mode;
+  op->prev_mode = op->mode;
 
   // mode check
   if(x == 0) {
-    key_count += (z<<1)-1;
+    op->key_count += (z<<1)-1;
 
-    if(key_count == 1 && z == 1)
-      mode = 1; 
-    else if(key_count == 0)
-      mode = 0;
+    if(op->key_count == 1 && z == 1)
+      op->mode = 1; 
+    else if(op->key_count == 0)
+      op->mode = 0;
 
-    if(z == 1 && mode == 1) {
-      edit_row = y;
+    if(z == 1 && op->mode == 1) {
+      op->edit_row = y;
       dirty_grid = 1;
-      // post("edit row:",edit_row);
+      // post("edit row:",op->edit_row);
     }
   }
-  else if(x == 1 && mode != 0) {
-    if(mode == 1 && z == 1)
-      mode = 2;
-    else if(mode == 2 && z == 0)
-      mode = 1;
+  else if(x == 1 && op->mode != 0) {
+    if(op->mode == 1 && z == 1)
+      op->mode = 2;
+    else if(op->mode == 2 && z == 0)
+      op->mode = 1;
   }
-  else if(mode == 0 && z == 1) {
-    points[y] = x;
-    points_save[y] = x;
-    positions[y] = x;
+  else if(op->mode == 0 && z == 1) {
+    op->points[y] = x;
+    op->points_save[y] = x;
+    op->positions[y] = x;
     dirty_grid = 1;
   }
-  else if(mode == 1 && z == 1) {
-    if(y != edit_row) {    // filter out self-triggering
-      trig_dests[edit_row] ^= (1<<y);
+  else if(op->mode == 1 && z == 1) {
+    if(y != op->edit_row) {    // filter out self-triggering
+      op->trig_dests[op->edit_row] ^= (1<<y);
       dirty_grid = 1;
-      // post("\ntrig_dests", edit_row, ":", trig_dests[edit_row]);
+      // post("\ntrig_dests", op->edit_row, ":", op->trig_dests[op->edit_row]);
     }
   }
-  else if(mode == 2 && z == 1) {
+  else if(op->mode == 2 && z == 1) {
     if(x > 1 && x < 6) {
-      rule_dests[edit_row] = y;
+      op->rule_dests[op->edit_row] = y;
       dirty_grid = 1;
-      // post("\nrule_dests", edit_row, ":", rule_dests[edit_row]);
+      // post("\nrule_dests", op->edit_row, ":", op->rule_dests[op->edit_row]);
     }
     else if(x > 5) {
-      rules[edit_row] = y;
+      op->rules[op->edit_row] = y;
       dirty_grid = 1;
-      // post("\nrules", edit_row, ":", rules[edit_row]);
+      // post("\nrules", op->edit_row, ":", op->rules[op->edit_row]);
     }
   }
 
-  if(mode != prev_mode) {
+  if(op->mode != op->prev_mode) {
     dirty_grid = 1;
     // post("\nnew mode", mode);
   }
@@ -336,6 +352,7 @@ static void op_cascades_handler(op_monome_t* op_monome, u32 edata) {
 
 // redraw monome grid
 static void op_cascades_redraw(op_monome_t* op_monome) {
+  op_cascades_t *op = (op_cascades_t *) op_monome->op;
   u8 i1, i2, i3;
 
   // clear grid
@@ -343,50 +360,50 @@ static void op_cascades_redraw(op_monome_t* op_monome) {
     op_monome->opLedBuffer[i1] = 0;
 
   // SET POSITIONS
-  if(mode == 0) {
+  if(op->mode == 0) {
     for(i1=0;i1<8;i1++) {
-      for(i2=positions[i1];i2<=points[i1];i2++)
+      for(i2=op->positions[i1];i2<=op->points[i1];i2++)
         op_monome->opLedBuffer[i1*16 + i2] = L1;
 
-      op_monome->opLedBuffer[i1*16 + positions[i1]] = L2;
+      op_monome->opLedBuffer[i1*16 + op->positions[i1]] = L2;
     }
   }
   // SET ROUTING
-  else if(mode == 1) {
-    op_monome->opLedBuffer[edit_row * 16] = L1;
-    op_monome->opLedBuffer[edit_row * 16 + 1] = L1;
+  else if(op->mode == 1) {
+    op_monome->opLedBuffer[op->edit_row * 16] = L1;
+    op_monome->opLedBuffer[op->edit_row * 16 + 1] = L1;
 
     for(i1=0;i1<8;i1++) {
-      if((trig_dests[edit_row] & (1<<i1)) != 0) {
-        for(i2=0;i2<=points[i1];i2++)
+      if((op->trig_dests[op->edit_row] & (1<<i1)) != 0) {
+        for(i2=0;i2<=op->points[i1];i2++)
           op_monome->opLedBuffer[i1*16 + i2] = L2;
       }
-      op_monome->opLedBuffer[i1*16 + positions[i1]] = L0;
+      op_monome->opLedBuffer[i1*16 + op->positions[i1]] = L0;
     }
   }
   // SET RULES
-  else if(mode == 2) {
-    op_monome->opLedBuffer[edit_row * 16] = L1;
-    op_monome->opLedBuffer[edit_row * 16 + 1] = L1;
+  else if(op->mode == 2) {
+    op_monome->opLedBuffer[op->edit_row * 16] = L1;
+    op_monome->opLedBuffer[op->edit_row * 16 + 1] = L1;
 
     for(i1=2;i1<7;i1++)
-      op_monome->opLedBuffer[rule_dests[edit_row] * 16 + i1] = L2;
+      op_monome->opLedBuffer[op->rule_dests[op->edit_row] * 16 + i1] = L2;
 
     for(i1=8;i1<16;i1++)
-      op_monome->opLedBuffer[rules[edit_row] * 16 + i1] = L0;
+      op_monome->opLedBuffer[op->rules[op->edit_row] * 16 + i1] = L0;
 
     for(i1=0;i1<8;i1++) 
-      op_monome->opLedBuffer[i1*16 + positions[i1]] = L0;
+      op_monome->opLedBuffer[i1*16 + op->positions[i1]] = L0;
 
     for(i1=0;i1<8;i1++) {
-      i3 = glyph[rules[edit_row]][i1];
+      i3 = glyph[op->rules[op->edit_row]][i1];
       for(i2=0;i2<8;i2++) {
         if((i3 & (1<<i2)) != 0)
           op_monome->opLedBuffer[i1*16 + 8 + i2] = L2;
       }
     }
 
-    op_monome->opLedBuffer[rules[edit_row] * 16 + 7] = L2;
+    op_monome->opLedBuffer[op->rules[op->edit_row] * 16 + 7] = L2;
   }
 
   monome_set_quadrant_flag(0);
@@ -407,6 +424,11 @@ static u32 rnd() {
 u8* op_cascades_pickle(op_cascades_t* mgrid, u8* dst) {
   dst = pickle_io(mgrid->focus, dst);
   dst = pickle_io(mgrid->size, dst);
+  u32 *mp_state = (u32*)mgrid->positions;
+  while ((u8*)mp_state < (u8*) &(mgrid->edit_row)) {
+    dst = pickle_32(*mp_state, dst);
+    mp_state +=1;
+  }
   /// no state...???
   return dst;
 }
@@ -415,6 +437,11 @@ const u8* op_cascades_unpickle(op_cascades_t* mgrid, const u8* src) {
   src = unpickle_io(src, (u32*)&(mgrid->focus));
   // FIXME should probably auto-detect grid size here::::
   src = unpickle_io(src, (u32*)&(mgrid->size));
+  u32 *mp_state = (u32*)mgrid->positions;
+  while ((u8*)mp_state < (u8*) &(mgrid->edit_row)) {
+    src = unpickle_32(src, mp_state);
+    mp_state +=1;
+  }
   /*
     probably shouldn't call this here...
    if we assume that network monome device focus is null during unpickling,
