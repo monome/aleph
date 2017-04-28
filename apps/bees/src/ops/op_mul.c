524eb53a2c0caae050e2aeaabede078a2e60b9bd
UrTih the Alchemist <UrTih@planet.thra>
2017-04-28 01:34:38 -0700

    fix bug in op_mul

--- apps/bees/src/ops/op_mul.c
+++ apps/bees/src/ops/op_mul.c
@@ -58,8 +58,8 @@ void op_mul_init(void* mem) {
 static void op_mul_in_a(op_mul_t* mul, const io_t v) {
   // printf("mul at %d received A %d\n", (int)mul, (int)*v);
   mul->a = v;
-  net_activate(mul, 0, mul->val);
   mul->val = op_mul(mul->a, mul->b);
+  net_activate(mul, 0, mul->val);
 }
 
 // set operand B
