7186438523ccc65b968899e654c0347fbcfbf2cc
UrTih the Alchemist <UrTih@planet.thra>
2017-05-01 18:18:28 -0700

    move kria_set inside op_kria struct

--- apps/bees/src/ops/op_kria.h
+++ apps/bees/src/ops/op_kria.h
@@ -7,10 +7,52 @@
 #include "op_math.h"
 #include "types.h"
 
+typedef enum {
+  mTr, mDur, mNote, mScale, mTrans, mScaleEdit, mPattern
+} modes;
+
+typedef enum {
+  tTr, tAc, tOct, tDur, tNote, tTrans, tScale
+} time_params;
+
+typedef enum {
+  modNone, modLoop, modTime
+} mod_modes;
+
+#define NUM_PARAMS 7
+
+const u8 SCALE[49];
+
+typedef struct {
+  u8 tr[16];
+  u8 ac[16];
+  u8 oct[16];
+  u8 note[16];
+  u8 dur[16];
+  u8 sc[16];
+  u8 trans[16];
+
+  u8 dur_mul;
+
+  u8 lstart[NUM_PARAMS];
+  u8 lend[NUM_PARAMS];
+  u8 llen[NUM_PARAMS];
+  u8 lswap[NUM_PARAMS];
+  time_params tmul[NUM_PARAMS];
+  // time_params tdiv[NUM_PARAMS];
+} kria_pattern;
+
+// TO 96
+typedef struct {
+  kria_pattern kp[2][16];
+  u8 pscale[7];
+} kria_set;
+
 //--- white whale
 typedef struct op_kria_struct {
   op_t super;
   op_monome_t monome;
+  kria_set k;
   // inputs: mode, focus, step
   volatile io_t focus;
   volatile io_t clk;
