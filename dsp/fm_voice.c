2ddb2f8e92095a06fbfc9db779314b51bae411f9
rick <sasquatch@rickvenn.com>
2017-10-30 11:09:03 -0700

    seemingly working oversampled fm

--- dsp/fm_voice.c
+++ dsp/fm_voice.c
@@ -35,44 +35,51 @@ void fm_voice_release (fm_voice *v) {
   }
 }
 
-#define FM_OVERSAMPLE_BITS 3
+#define FM_OVERSAMPLE_BITS 2
 #define FM_OVERSAMPLE (1 << FM_OVERSAMPLE_BITS)
 #define FM_SMOOTH ((fract16) (FR16_MAX * ((4.0 * PI * FM_OVERSAMPLE) / (1.0 + 4.0 * PI * FM_OVERSAMPLE))))
 
 void fm_voice_next (fm_voice *v) {
   int i, j;
   fract16 oversample_outs[FM_OPS_MAX][FM_OVERSAMPLE];
+
+  fract32 opFreqs[FM_OPS_MAX];
   fract16 envs[FM_OPS_MAX];
+  fract32 baseFreq = fix16_mul_fract(v->noteHz, v->noteTune);
   for(i=0; i < v->nOps; i++) {
     envs[i] = env_adsr_16_next(&(v->opEnv[i]));
+    opFreqs[i] = fix16_mul_fract(baseFreq, v->opTune[i]);
+    opFreqs[i] = shr_fr1x32(opFreqs[i], FM_OVERSAMPLE_BITS);
   }
+
   for(j=0; j < FM_OVERSAMPLE; j++) {
     fract16 nextOpOutputs[FM_OPS_MAX];
-    fract32 baseFreq = fix16_mul_fract(v->noteHz, v->noteTune);
     for(i=0; i < v->nOps; i++) {
-      fract32 opFreq = fix16_mul_fract(baseFreq, v->opTune[i]);
+      // calculate modulation point & bandlimit to 20kHz or so
       fract16 opMod = multr_fr1x16(v->opOutputs[v->opMod1Source[i]],
 				   v->opMod1Gain[i]);
       opMod = add_fr1x16(opMod,
 			 multr_fr1x16(v->opOutputs[v->opMod2Source[i]],
 				      v->opMod2Gain[i]));
-      opFreq = shr_fr1x32(opFreq, FM_OVERSAMPLE_BITS);
       opMod = mult_fr1x16(opMod, FR16_MAX - FM_SMOOTH);
       opMod = add_fr1x16(opMod,
       			 multr_fr1x16(v->opModLast[i], FM_SMOOTH));
       v->opModLast[i] = opMod;
-      fract32 opPhase = phasor_next_dynamic(&(v->opOsc[i]), opFreq);
+
+      // phase increment each op with the oversample-compensated frequency
+      fract32 opPhase = phasor_next_dynamic(&(v->opOsc[i]), opFreqs[i]);
       nextOpOutputs[i] = multr_fr1x16(envs[i],
-				      osc16(opPhase + shl_fr1x32(opMod, 18)));
+				      osc16(opPhase + shl_fr1x32(opMod, 20)));
     }
+
+    // set up the oversampled output to sum & average
     for(i=0; i < v->nOps; i++) {
-      v->opOutputs[i] = nextOpOutputs[i];
-      for(j=0; j < FM_OVERSAMPLE; j++) {
-	oversample_outs[i][j] = shr_fr1x16(nextOpOutputs[i],
-					   FM_OVERSAMPLE_BITS);
-      }
+      oversample_outs[i][j] = shr_fr1x16(nextOpOutputs[i],
+					 FM_OVERSAMPLE_BITS);
     }
   }
+
+  // convert the oversampled output back to original sample rate
   for(i=0; i < v->nOps; i++) {
     v->opOutputs[i] = 0;
     for(j=0; j < FM_OVERSAMPLE; j++) {
@@ -81,21 +88,3 @@ void fm_voice_next (fm_voice *v) {
     }
   }
 }
-
-
-#if 0
-
-void fm_voice_next (fm_voice *v) {
-  int i;
-  fract32 baseFreq = fix16_mul_fract(v->noteHz, v->noteTune);
-  for(i=0; i < v->nOps; i++) {
-    fract32 opFreq = fix16_mul_fract(baseFreq, v->opTune[i]);
-    opFreq  = shr_fr1x32(opFreq, FM_OVERSAMPLE_BITS);
-    fract32 opPhase = phasor_next_dynamic(&(v->opOsc[i]), opFreq);
-    v->opOutputs[i] = multr_fr1x16(env_adsr_16_next(&(v->opEnv[i])),
-    				   osc16(opPhase));
-    /* v->opOutputs[i] = osc16(opPhase); */
-  }
-}
-
-#endif
