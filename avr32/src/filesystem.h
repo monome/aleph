905daab117ecf432ddf345cc37e393168620389c
prgmsoftware <35396092+prgmsoftware@users.noreply.github.com>
2018-03-22 23:42:48 +0100

    Update filesystem.h

--- avr32/src/filesystem.h
+++ avr32/src/filesystem.h
@@ -8,8 +8,6 @@
 
 //=====================================
 //==== vars
-// transfer-done flag
-//extern volatile u8 fsEndTransfer;
 // Local RAM buffer to store data  to/from the SD/MMC card
 extern volatile U8 pdcaRxBuf[FS_BUF_SIZE];
 extern volatile U8 pdcaTxBuf[FS_BUF_SIZE];
@@ -20,8 +18,6 @@ extern volatile avr32_pdca_channel_t* pdcaTxChan;
 //====================================
 //==== funcs
 extern int fat_init(void);
-
-//PRGM SAMPLE TRANSFER PR
 extern int bfin_sample_transfer(unsigned long sector, unsigned long bytes);
 
 #endif  // h guard
