2858b97d18e8a28134d116e63cff5a169db79b59
Greg Wuller <greg@afofo.com>
2026-07-14 22:15:54 -0700

    fix(bfin_lib): change module.h defs to decls

--- bfin_lib/src/module.h
+++ bfin_lib/src/module.h
@@ -23,13 +23,13 @@
 
 
 #else
-void *SDRAM_ADDRESS;
+extern void *SDRAM_ADDRESS;
 #define IN_PORTS 4
 #define OUT_PORTS 4
 #define SDRAM_SIZE 0x07ffffff
 
-fract32 in[IN_PORTS];
-fract32 out[OUT_PORTS];
+extern fract32 in[IN_PORTS];
+extern fract32 out[OUT_PORTS];
 
 void module_init(void);
 void module_process_frame(void);
